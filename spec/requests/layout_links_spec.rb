d78a733db3ac41ebc20e0c5095c0acb313918685
RonaldEV <code@elesiasventures.com>
2010-09-05 22:31:31 +0100

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+  describe "initial layout" do
 
   it "should have a Home page at '/'" do
     get '/'
@@ -27,17 +28,52 @@ it "should have a Help page at '/signup'" do
 
 end
 
-    it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    response.should have_selector('title', :content => "About")
-    click_link "Help"
-    response.should # fill in
-    click_link "Contact"
-    response.should # fill in
-    click_link "Home"
-    response.should # fill in
-    click_link "Sign up now!"
-    response.should # fill in
+#    it "should have the right links on the layout" do
+#    visit root_path
+#    click_link "About"
+#    response.should have_selector('title', :content => "About")
+#    click_link "Help"
+#    response.should # fill in
+#    click_link "Contact"
+#    response.should # fill in
+#    click_link "Home"
+#    response.should # fill in
+#    click_link "Sign up now!"
+#    response.should # fill in
+#    end
+  end
+
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                          :content => "Sign in")
+
+    end
+  end
+
+  describe "when signed in " do
+
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email,    :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+                                        :content => "Sign out")
+    end
+
+
+
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                        :content => "Profile")
+    end
   end
 end
